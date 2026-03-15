825818e73ee5a2fa8f54f34632191103aff72e02
Tony Nie <tonywhitewhite@hotmail.com>
2026-03-15 15:54:13 +0800

    ci: add GitHub Pages deployment for project site

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { buildNumberPlugin } from './vite-plugin-build-number'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [vue(), buildNumberPlugin()],
   server: {
     host: '127.0.0.1',
